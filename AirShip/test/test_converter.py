3efe76275fc1a4e2dfa6eeac6b2b6ff15be4f7b0
timbohiatt <timbohiatt@gmail.com>
2024-04-08 13:50:13 +0000

    Reworking the Converter products from the initail MVP into a Scalable, Testable and Imoprtable Python Package

--- AirShip/test/test_converter.py
+++ AirShip/test/test_converter.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Copyright 2024 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -31,23 +30,3 @@ class TestClass(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-=======
-import unittest
-from converter import new_converter, ControlMConverter
-
-class TestClass(unittest.TestCase):
-	def test_new_converter_controlm_valid(self):
-		pass
-        # Test Control-M with Mixed Cases
-		#self.assertEqual(new_converter(converter_type="Control-M"), type(ControlMConverter))
-        # Test Control-M with All Lowercase
-		#con = new_converter(converter_type="control-m")
-        # Test Control-M with All Uppercase
-		#con = new_converter(converter_type="CONTROL-M")
-        # Test Control-M with Mixed Cases No Hyphen
-		#con = new_converter(converter_type="cOnTroLm")
-
-
-if __name__=='__main__':
-	unittest.main()
->>>>>>> 45ce3a1 (Reworking the Converter products from the initail MVP into a Scalable, Testable and Imoprtable Python Package)
