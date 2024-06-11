ebb6d02e66b19eb7a5ff2cc754c55d4f479f97f5
Konrad Schieban <kschieban@google.com>
2024-06-11 13:41:12 +0200

    new files for integration tests (#45)

--- dagify/test/integration/test_references/009-fast-x/fast_x.py
+++ dagify/test/integration/test_references/009-fast-x/fast_x.py
@@ -3,7 +3,7 @@ from airflow import DAG
 from airflow.decorators import task
 import datetime
 # Apache Airflow Custom & DAG/Task Specific Imports
-from airflow.operators.python import PythonOperator
+from airflow.providers.ssh.operators.ssh import SSHOperator
 
 default_args = {
     'owner': 'airflow',
@@ -12,51 +12,47 @@ default_args = {
 with DAG(
     dag_id="fast_x",
     start_date=datetime.datetime(2024, 1, 1),
-    # schedule="@daily",
-    schedule_interval='*/1 * * * *',
+    #schedule="@daily",
+    schedule_interval='*/5 * * * *',
+    catchup=False,
 ) as dag:
 
     # DAG Tasks
-    fast_x_job_1_95096 = PythonOperator(
-        task_id="fast_x_job_1_95096",
-        python_callable=helloWorld,
-        op_args=["fast_x_job_1_95096"],
-        dag=dag,
-    )
-    fast_x_job_2_7431b = PythonOperator(
-        task_id="fast_x_job_2_7431b",
-        python_callable=helloWorld,
-        op_args=["fast_x_job_2_7431b"],
-        dag=dag,
-    )
-    fast_x_job_3_a87ed = PythonOperator(
-        task_id="fast_x_job_3_a87ed",
-        python_callable=helloWorld,
-        op_args=["fast_x_job_3_a87ed"],
-        dag=dag,
-    )
-    fast_x_job_4_20442 = PythonOperator(
-        task_id="fast_x_job_4_20442",
-        python_callable=helloWorld,
-        op_args=["fast_x_job_4_20442"],
-        dag=dag,
-    )
-    fast_x_job_5_5fbd0 = PythonOperator(
-        task_id="fast_x_job_5_5fbd0",
-        python_callable=helloWorld,
-        op_args=["fast_x_job_5_5fbd0"],
-        dag=dag,
-    )
-    fast_x_job_6_65c50 = PythonOperator(
-        task_id="fast_x_job_6_65c50",
-        python_callable=helloWorld,
-        op_args=["fast_x_job_6_65c50"],
-        dag=dag,
+    fast_x_job_1 = SSHOperator(
+      task_id="fast_x_job_1",
+      command="",
+      dag=dag,
     )
 
+    fast_x_job_2 = SSHOperator(
+      task_id="fast_x_job_2",
+      command="",
+      dag=dag,
+    )
+
+    fast_x_job_3 = SSHOperator(
+      task_id="fast_x_job_3",
+      command="",
+      dag=dag,
+    )
+
+    fast_x_job_4 = SSHOperator(
+      task_id="fast_x_job_4",
+      command="",
+      dag=dag,
+    )
+
+    fast_x_job_5 = SSHOperator(
+      task_id="fast_x_job_5",
+      command="",
+      dag=dag,
+    )
+
+    fast_x_job_6 = SSHOperator(
+      task_id="fast_x_job_6",
+      command="",
+      dag=dag,
+    )
+
+
     # Airflow Task Dependencies
-    fast_x_job_1_95096 >> [fast_x_job_2_7431b, fast_x_job_3_a87ed]
-    fast_x_job_2_7431b >> [fast_x_job_3_a87ed, fast_x_job_5_5fbd0]
-    fast_x_job_3_a87ed >> [fast_x_job_6_65c50]
-    fast_x_job_4_20442 >> [fast_x_job_6_65c50]
-    fast_x_job_5_5fbd0 >> [fast_x_job_6_65c50]
--- dagify/test/integration/test_references/010-fast-x/fx_fld_001.py
+++ dagify/test/integration/test_references/010-fast-x/fx_fld_001.py
@@ -3,7 +3,7 @@ from airflow import DAG
 from airflow.decorators import task
 import datetime
 # Apache Airflow Custom & DAG/Task Specific Imports
-from airflow.operators.python import PythonOperator
+from airflow.providers.ssh.operators.ssh import SSHOperator
 
 default_args = {
     'owner': 'airflow',
@@ -12,30 +12,29 @@ default_args = {
 with DAG(
     dag_id="fx_fld_001",
     start_date=datetime.datetime(2024, 1, 1),
-    # schedule="@daily",
-    schedule_interval='*/1 * * * *',
+    #schedule="@daily",
+    schedule_interval='*/5 * * * *',
+    catchup=False,
 ) as dag:
 
     # DAG Tasks
-    fx_fld_001_app_001_subapp_001_job_001_ce606 = PythonOperator(
-        task_id="fx_fld_001_app_001_subapp_001_job_001_ce606",
-        python_callable=helloWorld,
-        op_args=["fx_fld_001_app_001_subapp_001_job_001_ce606"],
-        dag=dag,
-    )
-    fx_fld_001_app_001_subapp_001_job_002_06adf = PythonOperator(
-        task_id="fx_fld_001_app_001_subapp_001_job_002_06adf",
-        python_callable=helloWorld,
-        op_args=["fx_fld_001_app_001_subapp_001_job_002_06adf"],
-        dag=dag,
-    )
-    fx_fld_001_app_001_subapp_001_job_003_a8eda = PythonOperator(
-        task_id="fx_fld_001_app_001_subapp_001_job_003_a8eda",
-        python_callable=helloWorld,
-        op_args=["fx_fld_001_app_001_subapp_001_job_003_a8eda"],
-        dag=dag,
+    fx_fld_001_app_001_subapp_001_job_001 = SSHOperator(
+      task_id="fx_fld_001_app_001_subapp_001_job_001",
+      command="",
+      dag=dag,
     )
 
+    fx_fld_001_app_001_subapp_001_job_002 = SSHOperator(
+      task_id="fx_fld_001_app_001_subapp_001_job_002",
+      command="",
+      dag=dag,
+    )
+
+    fx_fld_001_app_001_subapp_001_job_003 = SSHOperator(
+      task_id="fx_fld_001_app_001_subapp_001_job_003",
+      command="",
+      dag=dag,
+    )
+
+
     # Airflow Task Dependencies
-    fx_fld_001_app_001_subapp_001_job_001_ce606 >> [fx_fld_001_app_001_subapp_001_job_002_06adf, fx_fld_001_app_001_subapp_001_job_003_a8eda]
-    fx_fld_001_app_001_subapp_001_job_002_06adf >> [fx_fld_001_app_001_subapp_001_job_003_a8eda]
