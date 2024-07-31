acaff858eeef9cc1ca11741577c62cabbbdd3d37
harish1203 <harishsridhar@google.com>
2024-07-31 18:01:54 +0530

    adding reporting functionality as separate module

--- dagify/converter/utils.py
+++ dagify/converter/utils.py
@@ -16,6 +16,8 @@ import re
 import os
 import yaml
 import pprint
+from prettytable import PrettyTable
+import xml.etree.ElementTree as ET
 
 
 def clean_converter_type(converter_type):
@@ -102,3 +104,55 @@ def display_dict(dict):
         dict (dict): The dictionary to print.
     """
     pprint.pprint(dict)
+
+def count_yaml_files(directory, case_sensitive=True, recursive=False):
+  """
+  Counts the number of YAML files (.yaml or .yml) in a directory.
+
+  Args:
+      directory (str): The path to the directory to search.
+      case_sensitive (bool): Whether the search should be case-sensitive (default: True).
+      recursive (bool): Whether to search subdirectories recursively (default: False).
+
+  Returns:
+      int: The number of YAML files found.
+  """
+
+  count = 0
+  for root, dirs, files in os.walk(directory):
+      for file in files:
+          if (case_sensitive and file.endswith(('.yaml', '.yml'))) or \
+             (not case_sensitive and file.lower().endswith(('.yaml', '.yml'))):
+              count += 1
+      if not recursive:
+          break  # Stop after the first level if not recursive
+
+  return count
+
+def generate_report(title,columns,rows,output_dir):
+        report = PrettyTable()
+        report.title = title
+        i=0
+        # Column config
+        report.field_names = columns
+        for col in columns:
+            report.align[col] = "l"
+
+        # Row config
+        report.add_rows(rows)
+
+
+        report_file = f"{output_dir}/Detailed-Report.txt"
+        with open(report_file, "w") as final_report:
+            final_report.write(str(report))
+
+def get_jobtypes_andcount(source_path):
+    tree = ET.parse(source_path)
+    root = tree.getroot()
+    # Find all JOB elements
+    job_elements = root.findall('.//JOB')
+    # Extract TASKTYPE values and store them in a set to ensure uniqueness
+    job_types_source = list({job.get('TASKTYPE') for job in job_elements})
+    job_types_count = len(job_types_source)
+
+    return job_types_source,job_types_count
