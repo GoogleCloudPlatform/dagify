e807dc56424aa924dd9b1d4a4cb4fe016d58ebe7
Shreya Prabhu <shreyaprabhu@google.com>
2024-10-28 22:40:26 +0530

    Fix grammar

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ if [ -n "$RUN_MODE" ]; then
     cd ui
     uvicorn app:app --host 0.0.0.0 --port 8000
   else
-    echo "Error: Invalid RUN_MODE value. Use 'ui' or omit the flag to run the CLI."
+    echo "Error: Invalid value for RUN_MODE. Use 'ui' or omit the flag to run the CLI."
     exit 1
   fi
 else
